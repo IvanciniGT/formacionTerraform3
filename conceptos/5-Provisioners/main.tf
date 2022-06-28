8c71eaf4b598c0784f0448dd34649ce142d36048
Ubuntu <ubuntu@ip-172-31-10-35.eu-west-1.compute.internal>
2022-06-28 12:56:41 +0000

    dia2

--- conceptos/5-Provisioners/main.tf
+++ conceptos/5-Provisioners/main.tf
@@ -4,10 +4,15 @@ terraform {
         docker = {
             source = "kreuzwerker/docker" 
         }
+        null = {
+            source = "hashicorp/null" 
+        }
     }
 }
 
 provider "docker" {
+}
+provider "null" {
 }
 
 resource "docker_container" "mi_contenedor" {
@@ -34,6 +39,76 @@ resource "docker_container" "mi_contenedor" {
     }
 }
 
+
+
+resource "docker_container" "mi_contenedor_multiple" {
+    count = 3
+    name  = "mi_apache_${count.index}"
+    image = docker_image.mi_imagen.latest 
+}
+
+resource "null_resource" "ejecutor" {
+    triggers = {
+    # Nos da relación de dependencia / ORDEN EN LA EJECUCION
+    # Que solo se ejecuta si hay cambio en los contenedores
+        mi_trigger = join( " ", docker_container.mi_contenedor_multiple.*.ip_address )
+    }
+    provisioner "local-exec" {
+        command = "echo HOLA"
+    }
+}
+
+
+
+
+
+resource "docker_container" "mi_contenedor_con_ejecucion_remota" {
+    name  = "mi_ssh_container"
+    image = docker_image.mi_imagen.latest 
+
+    # Conectarme primero con el entorno remoto
+    # IP
+    # usuario
+    # password / credenciales
+    # puerto
+    # metodo de conexion: ssh | winrm
+    connection {
+        user        =   "root"
+        password    =   "root"
+        type        =   "ssh"
+        port        =   22
+        host        =   self.ip_address
+    }
+    provisioner "remote-exec" {
+        inline = [
+                    "echo Hola desde el contenedor!!!",
+                    "echo Yo también te saludo desde el contenedor"
+                ]
+
+    }
+    provisioner "remote-exec" {
+        script = "mi_script.sh" # Copia y ejecuta un script
+    }
+    provisioner "remote-exec" {
+        scripts = ["mi_script.sh"]
+    }
+
+    provisioner "file" {
+        source      = "./configuracion.conf"
+        destination = "/tmp/miconfiguracion.txt"
+    }
+
+    provisioner "file" {
+        content     = "Aqui pongo el contenido: ${self.ip_address}"
+        destination = "/tmp/miconfiguracion2.txt"
+    }
+
+    
+}
+
+
+
+
 resource "docker_image" "mi_imagen" {
-    name = "httpd:alpine3.16"
+    name = "rastasheep/ubuntu-sshd:latest"
 }
